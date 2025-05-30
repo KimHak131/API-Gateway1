7e057b7f823d9f856d28a5b5058eb71cddb788a6
Kimhak Oul <137867485+KimHak131@users.noreply.github.com>
2025-05-30 21:37:24 +0700

    update local

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const proxy = httpProxy.createProxyServer({});
 // redirect to the student micro-service
 app.use('/student', (req, res) => {
     console.log("INSIDE API GATEWAY STUDENT ROUTE");
-    proxy.web(req, res, { target: 'http://35.175.152.246:4000' });
+    proxy.web(req, res, { target: 'http://localhost:4000' });
 });
 
 // redirect to the teacher micro-service
 app.use('/teacher', (req, res) => {
     console.log("INSIDE API GATEWAY TEACHER ROUTE");
-    proxy.web(req, res, { target: 'http://44.202.30.142:5000' });
+    proxy.web(req, res, { target: 'http://localhost:5000' });
 });
 
 // START THE EXPRESS SERVER. 3000 is the PORT NUMBER
